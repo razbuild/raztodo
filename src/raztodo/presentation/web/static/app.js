5ecf84f9bcfd2fe0ade30c4120060243548a71fe
Raz <real.raz.dev@gmail.com>
2026-06-23 18:15:43 +0330

    fix: escape quotes in HTML output to prevent attribute injection

--- src/raztodo/presentation/web/static/app.js
+++ src/raztodo/presentation/web/static/app.js
@@ -39,7 +39,9 @@ function esc(value) {
   return String(value)
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;");
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;");
 }
 
 function renderBadge(label, className = "") {
